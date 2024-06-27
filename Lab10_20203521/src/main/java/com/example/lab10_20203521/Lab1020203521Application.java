c9b452a7cd73ea8d2f61770383bff57b0e091a58
Julio Cesar <a20203521@pucp.edu.pe>
2024-06-26 19:40:50 -0500

    creacioon del proyecot

---
+++ Lab10_20203521/src/main/java/com/example/lab10_20203521/Lab1020203521Application.java
@@ -0,0 +1,13 @@
+package com.example.lab10_20203521;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1020203521Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1020203521Application.class, args);
+    }
+
+}
---
+++ Lab10_20203521/src/test/java/com/example/lab10_20203521/Lab1020203521ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab10_20203521;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1020203521ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
